57ad66db9ce73ffd8a4ac099e2fa5f641b2ea905
QuickTheo <theo.coutant32@gmail.com>
2020-03-28 17:18:47 +0100

    "first main created"

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome Home")
+}
+
+func main() {
+	fmt.Println("Hello World !")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
